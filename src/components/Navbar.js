081a7f706a87b9b20ea91f00305b81cc96774003
mikey1384 <mikey1384@gmail.com>
2019-03-03 19:52:00 +0700

    new css work started

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,30 +1,65 @@
 import React from 'react';
+import { css } from 'emotion';
 import { Link, NavLink } from 'react-router-dom';
 
 function Navbar() {
   return (
-    <nav className="navbar navbar-expand-lg navbar-light bg-light">
-      <Link className="navbar-brand" to="/">
-        Allan
-      </Link>
-      <div className="collapse navbar-collapse">
-        <ul className="navbar-nav">
-          <li className="nav-item">
-            <NavLink className="nav-link" activeClassName="active" exact to="/">
-              Home
-            </NavLink>
-          </li>
-          <li className="nav-item">
-            <NavLink className="nav-link" activeClassName="active" to="/chat">
-              Chat
-            </NavLink>
-          </li>
-          <li className="nav-item">
-            <NavLink className="nav-link" activeClassName="active" to="/videos">
-              Videos
-            </NavLink>
-          </li>
-        </ul>
+    <nav
+      className={css`
+        padding: 1rem;
+        display: flex;
+        justify-content: space-between;
+        a {
+          &:hover {
+            text-decoration: none;
+          }
+        }
+      `}
+    >
+      <div style={{ display: 'flex', width: '100%' }}>
+        <Link
+          style={{
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
+            fontSize: '2rem',
+            fontWeight: 'bold'
+          }}
+          to="/"
+        >
+          Allan
+        </Link>
+        <div>
+          <ul style={{ display: 'flex' }}>
+            <li>
+              <NavLink
+                className="nav-link"
+                activeClassName="active"
+                exact
+                to="/"
+              >
+                Home
+              </NavLink>
+            </li>
+            <li>
+              <NavLink className="nav-link" activeClassName="active" to="/chat">
+                Chat
+              </NavLink>
+            </li>
+            <li>
+              <NavLink
+                className="nav-link"
+                activeClassName="active"
+                to="/videos"
+              >
+                Videos
+              </NavLink>
+            </li>
+          </ul>
+        </div>
+      </div>
+      <div>
+        <button>Log In</button>
       </div>
     </nav>
   );
