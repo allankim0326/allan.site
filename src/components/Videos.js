b1da7628f610f98d3f6c35665fe1d8509dbd2026
allankim0326 <allankim0326@gmail.com>
2019-07-19 14:36:37 +0900

    the videos part

--- src/components/Videos.js
+++ src/components/Videos.js
@@ -4,18 +4,7 @@ import Video from './Video';
 export default function Videos() {
   return (
     <div>
-      <Video src="jhLXPFZpF-A" />
-      <Video src="9zIWQ9bFuXU" />
-      <Video src="N-auApltX_8" />
-      <Video src="ovbWlrGWva0" />
-      <Video src="I4xhOKC7q0U" />
-      <Video src="9uizYc2BIrM" />
-      <Video src="CFIPzLh-qIM" />
-      <Video src="_16CTlJ-lNI" />
-      <Video src="IWtWo2BJkHM" />
-      <Video src="cik0xQX9n80" />
-      <Video src="dDhYzPBVaZs" />
-      <Video src="rVAeaIafIxA" />
+      <Video src="jMrn3-hDUqQ" />
     </div>
   );
 }
